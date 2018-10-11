78af2bce71af82f0466924d9b49da27b795a28d3
Suann's Mac <Sueey@Su-3.local>
2018-10-11 14:29:14 -0400

    bare bones grid stuff

---
+++ grid.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+
+
+
+
+void initGrid(uint32_t rows, uint32_t cols){
+    for (uint32_t i=0; i<rows; i++) {
+        for (uint32_t j=0; j<cols; j++) {
+            ;
+        }
+    }
+}
+
+void freeGrid(grid toFree){
+    ;
+}
+
---
+++ grid.hpp
@@ -0,0 +1,10 @@
+#include <vector>
+
+
+
+class 
+//initialize the grid with hardcoded values depending on level
+void initGrid(uint32_t rows, uint32_t cols);
+
+//free grid's memory
+void freeGrid(grid toFree);
