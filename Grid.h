8b201c7940741d6b3ff9ce04909ad00818b54469
Suann's Mac <Sueey@Su-3.local>
2018-10-13 16:36:19 -0400

    Grid stuff compiles, writing it into GameMode to test if it actually works

--- Grid.h
+++ Grid.h
@@ -4,21 +4,31 @@
 #pragma once
 
 #include <cstdint>
+#include <string>
+#include <iostream>
+#include <vector>
 
 class Grid {
 public:
     class Coordinate{
-        Coordinate();
-        Coordinate(uint32_t x, uint32_t y);
+        //Coordinate();
+        //Coordinate(uint32_t x, uint32_t y);
+        public:
+            uint32_t x;
+            uint32_t y;
     };
 
     // Grid is initialized afterward.
     Grid();
     ~Grid();
 
+    //number of rows and cols in grid set here
+    uint32_t rows;
+    uint32_t cols;
+
     // Init the grid.
     // Maybe loading from some other files?
-    void initGrid();
+    void initGrid(std::string floorPlan);
 
     // Grid is only readable. It can't be modified after being initialized
     // Grid grid;
@@ -26,6 +36,7 @@ public:
     char at(Coordinate coor);
 
 private:
-    char** map;
+    std::vector< std::vector< char > > map;
+    //char map[rows][cols];
 };
 
