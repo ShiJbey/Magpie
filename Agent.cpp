7ca7d28fb9f768358d77be3e7de6f0a2170fc0c0
YorkHe <kiddmagician@gmail.com>
2018-10-15 19:32:51 -0400

    add classes: Entity, Model, Agent

---
+++ Agent.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 何宇 on 2018/10/15.
+//
+
+#include "Agent.h"
---
+++ Model.h
@@ -0,0 +1,17 @@
+//
+// Created by 何宇 on 2018/10/15.
+//
+
+#ifndef MAGPIE_MODEL_H
+#define MAGPIE_MODEL_H
+
+#include "Agent.h"
+
+class Model {
+public:
+    void draw(Agent* agent);
+    void update(float elapsed);
+};
+
+
+#endif //MAGPIE_MODEL_H
